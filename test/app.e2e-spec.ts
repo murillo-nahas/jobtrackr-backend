beacf916309eabc24b8b59ea40aa9afbbf8219b4
Murillo Nahás <murillonahas@hotmail.com>
2025-11-27 20:59:08 -0300

    feat: tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,13 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
+describe('Auth (e2e)', () => {
+  let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -16,10 +15,34 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/auth/register (POST)', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/auth/register')
+      .send({
+        name: 'Test User',
+        email: 'test@test.com',
+        password: '123456',
+      })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.access_token).toBeDefined();
+      });
+  });
+
+  it('/auth/login (POST)', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: 'test@test.com',
+        password: '123456',
+      })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.access_token).toBeDefined();
+      });
   });
 });
